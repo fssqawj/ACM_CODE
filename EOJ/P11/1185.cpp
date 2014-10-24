3159a86f747ddd257e17a6db182acac52306cd59
安炜杰 <fssqawj@163.com>
2014-10-24 19:07:54 +0800

    Create 1185.cpp

---
+++ EOJ/P11/1185.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int x,y,t;
+	scanf("%d",&t);
+	while(t--){
+	      scanf("%d%d",&x,&y);
+		  x>=y?printf("MMM BRAINS\n"):printf("NO BRAINS\n");
+	}
+	return 0;
+}
